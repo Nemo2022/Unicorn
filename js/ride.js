b04c0f627c6f332b4011726df919f8a5fb1ade21
Nemo2022 <91921795+Nemo2022@users.noreply.github.com>
2022-12-11 19:26:33 -0600

    change

--- js/ride.js
+++ js/ride.js
@@ -47,7 +47,7 @@ let map;
     function completeRequest(result, pickupLocation) {
         var unicorn;
         var pronoun;
-
+        getWeather(pickupLocation)
         console.log('Response received from API: ', result);
         unicorn = result.Unicorn;
         pronoun = unicorn.Gender === 'Male' ? 'his' : 'her';
@@ -55,7 +55,8 @@ let map;
 
         console.log(pickupLocation);
         //  get the local weather, find nearby restaurants, movies
-        // getWeather(pickupLocation, unicorn)
+        let searchText = document.getElementById('search').value;
+        getWeather(pickupLocation, unicorn)
 
         animateArrival(function animateCallback() {
             displayUpdate(unicorn.Name + ' has arrived. Giddy up!', unicorn.Color);
@@ -179,3 +180,91 @@ function displayUpdate(text, color='green') {
     $('#updates').prepend($(`<li style="background-color:${color}">${text}</li>`));
 }
 
+function getWeather(loc, unicorn) {
+    let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${loc.latitude}&lon=${loc.longitude}&exclude=minutely,hourly&appid=a099a51a6362902523bbf6495a0818aa`;
+    fetch(url)
+        .then(response => response.json())  //  wait for the response and convert it to JSON
+        .then(weather => {                  //  with the resulting JSON data do something
+
+            //  If the city was entered extract weather based on that API else use the LatLon API result format
+            let wx = latLonToWeather(weather);
+            let innerHTML = '';
+            let msg;
+            //  We have converted the Lon Lat API (onecall) and City API (forecast) requests to the same format
+            //  let's build a nice card for each day of the weather data
+            //  this is a GREAT opportunity to Reactify this code. But for now I will keep it simple
+            innerHTML += `<h4>Date: ${wx.daily[0].date}</h4>
+                        <h5>Temp: Low ${wx.daily[0].min}&deg; / High: ${wx.daily[0].max}&deg;</h5>
+                        <p>Forecast: <img src='https://openweathermap.org/img/wn/${wx.daily[0].icon}@2x.png' alt=""> ${wx.daily[0].description}</p>
+                        <p>Chance of rain at ${wx.daily[0].pop}%</p>
+                        <p>Wind at ${wx.daily[0].wind_speed} mph out of the ${wx.daily[0].windDirection}</p>
+                        <p>Sunrise: ${wx.daily[0].sunrise} / Sunset: ${wx.daily[0].sunset}</p>`;
+            displayUpdate(innerHTML, unicorn.Color);
+
+            msg =  `Temp is ${KtoF(weather.current.temp)} degrees,
+                        Wind at ${weather.current.wind_speed} miles per hour, 
+                        out of the ${windDirection(weather.current.wind_deg, true)}`;
+            // `${niceDate(weather.current.dt, weather.timezone_offset)}
+            // ${niceTime(weather.current.dt, weather.timezone_offset)}
+            // Temp is ${KtoF(weather.current.temp)} degrees,
+            // Wind at ${weather.current.wind_speed} miles per hour, out of the ${windDirection(weather.current.wind_deg, true)} ,
+            // Sunset will be at ${niceTime(weather.current.sunset, weather.timezone_offset)}`
+            console.log(msg);
+            speak(`Temp is ${KtoF(weather.current.temp)} degrees`);
+        });
+}
+
+function speak(msg) {
+    let speech = new SpeechSynthesisUtterance();
+    speech.lang = "en-US";
+    speech.text = msg;      //  TODO msg;
+    speech.volume = speech.rate = speech.pitch = 1;
+    window.speechSynthesis.speak(speech);
+}
+
+function latLonToWeather(data) {
+    let wx = {};
+    wx.daily = data.daily.map(d => ({
+        date:           niceDate(d.dt,data.timezone_offset),
+        min:            KtoF(d.temp.min),
+        max:            KtoF(d.temp.max),
+        sunrise:        niceTime(d.sunrise, data.timezone_offset),
+        sunset:         niceTime(d.sunset,  data.timezone_offset),
+        icon:           d.weather[0].icon,
+        description:    d.weather[0].description,
+        wind_speed:     d.wind_speed.toFixed(0),
+        windDirection:  windDirection(d.wind_deg, true),
+        pop:            (d.pop * 100).toFixed(0),
+        feels_like:     KtoF(d.feels_like.day),
+        dewPoint:       d.dew_point,
+        humidity:       d.humidity,
+    }));
+    wx.city = "";
+    wx.lat  = data.lat;
+    wx.lon  = data.lon;
+    return wx;
+}
+
+//  strip out just the MM/DD/YYY from the date
+//  convert from UNIX date time and take the time zone offset into consideration
+function niceDate(date, offset) {
+    let day = new Date(date * 1000 + offset);
+    day = day.toLocaleString();
+    return day.substring(0, 10);
+}
+
+//  Strip out just the HH:MM:SS AM/PM from the date
+function niceTime(dateTime, offset) {
+    let day = new Date(dateTime * 1000 + offset).toLocaleString();
+    let hour = day.indexOf(' ') + 1;
+    let time = day.substring(hour);
+    time = time.substring(0, time.lastIndexOf(':')) + time.substring(time.length-3)
+    return time;
+}
+
+//  Convert Kelvin to Fahrenheit
+function KtoF(temp) {
+    temp -= 273;
+    temp = temp * 9 / 5 + 32;
+    return temp.toFixed(0);
+}
